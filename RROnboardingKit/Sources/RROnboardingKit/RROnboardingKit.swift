73184a9ce4d2dda6d175bbbecae7e8816bcdc68b
Rudrank Riyam <rudrankriyam@gmail.com>
2020-10-11 00:25:22 +0530

    Add IntroductionView and TitleView

--- RROnboardingKit/Sources/RROnboardingKit/RROnboardingKit.swift
+++ RROnboardingKit/Sources/RROnboardingKit/RROnboardingKit.swift
@@ -1,3 +1,55 @@
-struct RROnboardingKit {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct RROnboardingKit {
+    public struct IntroductionView: View {
+        var title: String
+        var accentColor: Color
+
+        public init (title: String, accentColor: Color) {
+            self.title = title
+            self.accentColor = accentColor
+        }
+
+        public var body: some View {
+            ScrollView {
+                TitleView(title: title, accentColor: accentColor)
+            }
+        }
+    }
+
+    public struct TitleView: View {
+        var title: String
+        var accentColor: Color
+
+        public var body: some View {
+            VStack {
+                Text("WELCOME TO")
+                    .customTitleText(size: 30)
+
+                Text(title.uppercased())
+                    .customTitleText(size: 30)
+                    .foregroundColor(accentColor)
+            }
+            .accessibility(addTraits: .isHeader)
+            .padding(.horizontal)
+        }
+    }
+}
+
+extension Text {
+    func customTitleText(size: CGFloat) -> some View {
+        self
+            .fontWeight(.black)
+            .modifier(ScaledFont(size: size))
+    }
+}
+
+struct ScaledFont: ViewModifier {
+    @Environment(\.sizeCategory) var sizeCategory
+    var size: CGFloat
+
+    func body(content: Content) -> some View {
+        let scaledSize = UIFontMetrics.default.scaledValue(for: size)
+        return content.font(.system(size: scaledSize))
+    }
 }
--- RROnboardingKit/Tests/RROnboardingKitTests/RROnboardingKitTests.swift
+++ RROnboardingKit/Tests/RROnboardingKitTests/RROnboardingKitTests.swift
@@ -1,12 +1,12 @@
 import XCTest
+import SwiftUI
 @testable import RROnboardingKit
 
 final class RROnboardingKitTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RROnboardingKit().text, "Hello, World!")
+        var body: some View {
+            RROnboardingKit.IntroductionView(title: "Gradient Game", accentColor: Color(.systemIndigo))
+        }
     }
 
     static var allTests = [
